622d9427f186161f1623bc19456c07485b5e5370
MarkoSimka <63017129+MarkoSimka@users.noreply.github.com>
2021-07-01 23:39:05 +0200

    Upgrade
    Models added Domain and Identity
    Controllers added AccountController
    Domain added Product Class
    Identity added BiletiUserApplication

--- Kino_bileti/Kino_bileti/Startup.cs
+++ Kino_bileti/Kino_bileti/Startup.cs
@@ -1,4 +1,5 @@
 using Kino_bileti.Data;
+using Kino_bileti.Models.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,7 +31,7 @@ namespace Kino_bileti
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<BiletiUserApplication>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
